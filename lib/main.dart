d0ced7ed7fcd27a195b29d926ed3edc9c1278f5f
Bader AL Shehri <bader.shehri@gmail.com>
2024-06-17 11:49:30 +0300

    Add counter and many things
    Add counter and restructured project folder fix testing folder fix github sync fix docker testing in all devices

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,39 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Update for docker!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('$_counter'), // القيمة الأولية للنص
+              const SizedBox(height: 20),
+              FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ test/widget_test.dart
@@ -0,0 +1,34 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nawa_flutter_app/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // بناء الـwidget وإدخاله في الـWidget tree.
+    await tester.pumpWidget(const MainApp());
+
+    // التحقق من أن هناك نص "0" في الشجرة.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // البحث عن زر الأيقونة.
+    final Finder fab = find.byIcon(Icons.add);
+
+    // الضغط على الزر.
+    await tester.tap(fab);
+
+    // إعادة بناء الـWidget tree.
+    await tester.pump();
+
+    // التحقق من أن هناك نص "1" في الشجرة بعد الضغط على الزر.
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+  });
+}
