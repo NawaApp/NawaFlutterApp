b50f0fb9766f84302136768d76080f1d2f458fe3
Bader AL Shehri <bader.shehri@gmail.com>
2024-06-22 19:05:58 +0300

    google ads

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d15d56c1bbc026c0abd00d010bcea092",
+const RESOURCES = {"flutter_bootstrap.js": "1a337347f80532aa3752c0f9868ec19a",
 "version.json": "57441246a493e6647b6f359e22758238",
-"index.html": "c41dff6e057b5b9562e9ebbbe003707f",
-"/": "c41dff6e057b5b9562e9ebbbe003707f",
+"index.html": "a82f156c216ad21ab8d30ca13d28caea",
+"/": "a82f156c216ad21ab8d30ca13d28caea",
 "firebase-messaging-sw.js": "854f4682d9af1b5621acf54ab4a540f6",
 "main.dart.js": "d33ca7ffe824380bfbddd7665af1413e",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
